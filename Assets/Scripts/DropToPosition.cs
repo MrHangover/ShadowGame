c5534050205bf7ea316c1701eb1d11e774a77567
mogli88 <julialp@gmx.de>
2015-11-28 20:43:54 +0100

    Little Mermaid Level + Platform Drop Script

--- Assets/Scripts/DropToPosition.cs
+++ Assets/Scripts/DropToPosition.cs
@@ -5,16 +5,30 @@ public class DropToPosition : MonoBehaviour {
 
     Vector3 desiredPosition;
     float delay;
-    float timeToPosition;
+    public float timeToPosition =1f;
     float tempTime = 0f;
+
+    public bool isFalling = false;
+    public bool isPoppingUp = false;
 
 	// Use this for initialization
 	void Start () {
+
+        if (isFalling) { 
         desiredPosition = transform.position;
         transform.position = new Vector3(transform.position.x, 35f, transform.position.z);
-        timeToPosition = Random.value * 2f + 0.5f;
+        //timeToPosition = Random.value * 2f + 0.5f;
         delay = Random.value / 0.65f + 0.5f;
-	}
+        }
+
+        if (isPoppingUp)
+        {
+            desiredPosition = transform.position;
+            transform.position = new Vector3(transform.position.x, -35f, transform.position.z);
+            //timeToPosition = Random.value * 2f + 0.5f;
+            delay = Random.value / 0.65f + 0.5f;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
