c466add37fd2a07bdb41d65895d8ec8f15418ebb
mogli88 <julialp@gmx.de>
2015-12-05 20:09:26 +0100

    FadingScenes

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -18,14 +18,18 @@ public class FinishPoint : MonoBehaviour {
 		mat.SetTextureOffset("_MainTex", new Vector2(Mathf.Cos(Time.time) * 1f, Mathf.Sin(Time.time) * 1f));
 	}
 	
-	void OnTriggerEnter(Collider col){
+	IEnumerator OnTriggerEnter(Collider col){
 		if(col.gameObject.tag == "Player"){
             audio.PlayOneShot(finish);
             if (Application.levelCount > Application.loadedLevel + 1){
-				Application.LoadLevel(Application.loadedLevel + 1);
+                float fadeTime = GameObject.Find("Player").GetComponent<FadingScenes>().BeginFade(1);
+                yield return new WaitForSeconds(fadeTime);
+                Application.LoadLevel(Application.loadedLevel + 1);
 			}
 			else{
-				Application.LoadLevel(0);
+                float fadeTime = GameObject.Find("Player").GetComponent<FadingScenes>().BeginFade(1);
+                yield return new WaitForSeconds(fadeTime);
+                Application.LoadLevel(0);
 			}
 			Debug.Log("We have liftoff!");
 		}
